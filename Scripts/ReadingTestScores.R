5c2898bd6fc3a78139135e2d61d02322a52d215f
jimmy5589 <ahhomem@gmail.com>
2016-04-29 00:11:51 +0200

    DIEGEM Fri Apr 29 00:11:51 CEST 2016

--- Scripts/ReadingTestScores.R
+++ Scripts/ReadingTestScores.R
@@ -10,7 +10,7 @@ pisaTrain = na.omit(pisaTrain)
 
 pisaTest = na.omit(pisaTest)
 
-# Unordered factors ina linear regression, how to:
+# Unordered factors in linear regression, how to:
 # As an example, consider the unordered factor variable "color", 
 # with levels "red", "green", and "blue". 
 # If "green" were the reference level, then we would add binary variables "colorred" 
@@ -18,4 +18,41 @@ pisaTest = na.omit(pisaTest)
 # All red examples would have colorred=1 and colorblue=0. 
 # All blue examples would have colorred=0 and colorblue=1. 
 # All green examples would have colorred=0 and colorblue=0.
+
+#relevel to the most common
+
+pisaTrain$raceeth = relevel(pisaTrain$raceeth, "White")
+
+pisaTest$raceeth = relevel(pisaTest$raceeth, "White")
+
+# Create a model based in all independent variables
+
+lmScore= lm(readingScore ~ ., data=pisaTrain)
+summary(lmSquare)
+
+# Note that this R-squared is lower than the ones for the models we saw in the lectures
+# and recitation. This does not necessarily imply that the model is of poor quality. 
+# More often than not, it simply means that the prediction problem at hand 
+# (predicting a student's test score based on demographic and school-related variables) 
+# is more difficult than other prediction problems (like predicting a team's number of wins 
+# from their runs scored and allowed, or predicting the quality of wine from weather 
+# conditions).
+
+RMSE=sqrt(mean(lmScore$residuals^2))
+
+
+predTest=predict(lmScore, newdata=pisaTest)
+
+# Diff between Max and Min scores
+max(predTest)-min(predTest)
+
+#Problem 4.3
+SSE=sum((predTest-pisaTest$readingScore)^2)
+RMSE=sqrt(SSE/nrow(pisaTest))
+
+#Problem 4.3
+# Predicted Test Score used in baseline model.
+PredictedTestScore=mean(pisaTrain$readingScore)
+
+SST=sum((PredictedTestScore-pisaTest$readingScore)^2)
 
