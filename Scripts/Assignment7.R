689c12d819a91505c17aca1f074d7f4b9f1785fb
jimmy5589 <ahhomem@gmail.com>
2016-06-03 00:26:06 +0200

    MBP Fri Jun  3 00:26:06 CEST 2016

--- Scripts/Assignment7.R
+++ Scripts/Assignment7.R
@@ -33,4 +33,59 @@ length(table(statesMap$region))
 
 ggplot(predictionMap, aes(x = long, y = lat, group = group, fill = TestPredictionBinary)) + geom_polygon(color = "black")
 
+ggplot(predictionMap, aes(x = long, y = lat, group = group, fill = TestPredictionBinary))+ geom_polygon(color = "black") + scale_fill_gradient(low = "blue", high = "red", guide = "legend", breaks= c(0,1), labels = c("Democrat", "Republican"), name = "Prediction 2012")
 
+ggplot(predictionMap, aes(x = long, y = lat, group = group, fill = TestPrediction))+ geom_polygon(color = "black") + scale_fill_gradient(low = "blue", high = "red", guide = "legend", name = "Prediction 2012")
+
+ggplot(predictionMap, aes(x = long, y = lat, group = group, fill = TestPrediction))+ geom_polygon(color = "black", linetype=3) + scale_fill_gradient(low = "blue", high = "red", guide = "legend", name = "Prediction 2012")
+
+ggplot(predictionMap, aes(x = long, y = lat, group = group, fill = TestPrediction))+ geom_polygon(color = "black", size=3) + scale_fill_gradient(low = "blue", high = "red", guide = "legend", name = "Prediction 2012")
+
+
+Test
+TestPrediction[which(Test$State == "Florida")]
+
+############
+# Assign 2
+############
+
+edges = read.csv("edges.csv")
+users = read.csv("users.csv")
+
+str(users)
+str(edges)
+head(edges)
+table(edges$V1, edges$V2)
+rsum = rowSums(table(edges$V1, edges$V2))
+rsum
+csum = colSums(table(edges$V1, edges$V2))
+csum
+mean(rsum)
+mean(csum)
+edges[edges$V1==3981,]
+edges[edges$V1==4009,]
+edges[edges$V2==4009,]
+edges[edges$V2==3981,]
+
+rsum
+csum
+all=c(rsum,csum)
+allsum=tapply(unlist(all), names(unlist(all)), sum)
+allsum
+mean(allsum)
+length(allsum)
+sum(allsum)/59
+
+# From str(edges) or nrow(edges), we see that there are 146 pairs of users in our dataset 
+# who are Facebook friends. However, each pair (A, B) must be counted twice, 
+# because B is a friend of A and A is a friend of B. To think of this in simpler terms, 
+# consider a network with just new people, A and B, and a single edge (A, B). 
+# Even though there are two vertices and one edge, each user has on average one friend.
+
+# For our network, the average number of friends per user is 292/59=4.95.
+
+
+head(users)
+table(users$school, users$locale)
+table(users$gender, users$school)
+users
