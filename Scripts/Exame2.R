84362b910374139e09ef2439163da17e7073096e
jimmy5589 <ahhomem@gmail.com>
2016-07-01 00:27:05 +0200

    MBP Fri Jul  1 00:27:05 CEST 2016

--- Scripts/Exame2.R
+++ Scripts/Exame2.R
@@ -4,15 +4,11 @@ Hh=read.csv("Households.csv")
 summary(Hh)
 str(Hh)
 
-table(Hh$MorningPct > 0 & Hh$AfternoonPct == 0 )
+table(Hh$MorningPct == 100)
 
-table(Hh$MorningPct == 0 & Hh$AfternoonPct > 0 )
+table(Hh$AfternoonPct == 100 )
 
-nrow(Hh[Hh$MorningPct > 0 & Hh$AfternoonPct == 0,] )
-nrow(Hh[Hh$MorningPct == 0 & Hh$AfternoonPct > 0,] )
 
 head(Hh)
 
 
-nrow(Hh[Hh$AfternoonPct == 0,])
-nrow(Hh[Hh$MorningPct == 0,])
