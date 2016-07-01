483d384b3bdf7e404de369fb79b6948ec88b010b
jimmy5589 <ahhomem@gmail.com>
2016-07-01 16:41:04 +0200

    DIEGEM Fri Jul  1 16:41:04 METDST 2016

--- Scripts/Exame2.R
+++ Scripts/Exame2.R
@@ -11,4 +11,12 @@ table(Hh$AfternoonPct == 100 )
 
 head(Hh)
 
+Hh[Hh$AvgSalesValue > 150,]
+min(Hh[Hh$AvgSalesValue > 150,]$AvgDiscount)
+
+Hh[Hh$AvgDiscount > 25,]
+min(Hh[Hh$AvgDiscount > 25,]$AvgSalesValue)
+
+nrow(Hh[Hh$NumVisits >= 300,])/nrow(Hh)
+
 
